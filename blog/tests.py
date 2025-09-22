3024a0960f6e826598b4f12b87b35b56a13bfb89
rahul98076 <rahul98076@gmail.com>
2025-09-23 02:40:53 +0530

    test: Add initial tests for Post model and homepage view

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+
+class PostTests(TestCase):
+    # This special method runs before each test function.
+    # We use it to set up the data our tests will need.
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="password123"
+        )
+        self.post = Post.objects.create(
+            title="A Test Post Title",
+            slug="a-test-post",
+            author=self.user,
+            content="Some test content.",
+            status="published",
+        )
+
+    # All test methods must start with 'test_'.
+    def test_post_model_str(self):
+        """Test the string representation of the Post model."""
+        self.assertEqual(str(self.post), "A Test Post Title")
+
+    def test_homepage_status_code(self):
+        """Test that the homepage loads correctly and returns a 200 status code."""
+        # self.client is a dummy web browser we can use for testing.
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_contains_post(self):
+        """Test that a published post's title appears on the homepage."""
+        response = self.client.get("/")
+        self.assertContains(response, "A Test Post Title")
